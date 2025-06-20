fac51d5cdbb2a0a18d340a00542592cfd579e740
smalepati99 <srikanth.malepati1@digital.justice.gov.uk>
2025-06-20 09:59:14 +0100

    MAN-626 - Add automated tests for verifying NDelius and OASys risk data on MPoP Risk Page

--- steps/mpop/utils.ts
+++ steps/mpop/utils.ts
@@ -13,7 +13,6 @@ export const mpopShortMonthFormat = (date: Date) => {
 }
 
 export const today = DateTime.fromISO(DateTime.now())
-
 export const yesterday = today.minus({ days: 1 })
 export const plus3Months = today.plus({ months: 3 })  // => 12 Sep 2025
 export const plus6Months = today.plus({ months: 6 })  // => 12 Dec 2025
