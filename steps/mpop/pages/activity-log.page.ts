e5789bcaf6393ab9c82877324b9dd1c51d6bc266
Aidan Filby <Aidan.Filby@tpximpact.com>
2025-11-11 15:25:14 +0000

    activity log page

--- steps/mpop/pages/activity-log.page.ts
+++ steps/mpop/pages/activity-log.page.ts
@@ -2,11 +2,43 @@ import { expect, Page } from "@playwright/test";
 import MPopPage from "./page";
 
 export default class ActivityLogPage extends MPopPage {
-    constructor(page: Page) {
+    view : string = "default"
+
+    constructor(page: Page, view: string = "default") {
         super(page, "Activity log")
+        this.view = view
     }
 
     async goTo(crn: string){
-       await this.page.goto(`https://manage-people-on-probation-dev.hmpps.service.justice.gov.uk/case/${crn}/activity-log/`)
+       await this.page.goto(`https://manage-people-on-probation-dev.hmpps.service.justice.gov.uk/case/${crn}/activity-log/${this.view === "default" ? "" : `?view=${this.view}`}`)
     }
+
+    async changeView(){
+        if (this.view === "default"){
+            this.clickLink("Compact view")
+            this.view = "compact"
+        } else {
+            this.clickLink("Default view")
+            this.view = "default"
+        }
+    }
+
+    getTimelineCard(id: number){
+        return this.getQA(`timeline${id}Card`)
+    }
+
+    async fillText(qa: string, text: string){
+       await this.getQA(qa).locator('[type="text"]').fill(text)
+    }
+
+    async toggleComplianceFilter(id: number){
+        const filter = this.getClass("govuk-checkboxes__item", this.getQA("compliance")).nth(id).getByRole("checkbox")
+        if (filter.isChecked()){
+            await filter.uncheck()
+        } else {
+        await filter.check()
+        }
+    }
+
+    
 }
