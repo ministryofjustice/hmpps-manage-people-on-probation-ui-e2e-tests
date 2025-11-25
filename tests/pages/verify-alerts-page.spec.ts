177cdc98ddda31ac49245b5d637b04a06e29cb05
Aidan Filby <Aidan.Filby@tpximpact.com>
2025-11-25 12:29:44 +0000

    tests for all alerts page work

--- tests/pages/verify-alerts-page.spec.ts
+++ tests/pages/verify-alerts-page.spec.ts
@@ -1,4 +1,4 @@
-import { Browser, BrowserContext, expect, Page, test } from '@playwright/test'
+import { Browser, BrowserContext, expect, Locator, Page, test } from '@playwright/test'
 import * as dotenv from 'dotenv'
 import { navigateToAlerts } from '../../steps/mpop/navigation/base-navigation.ts'
 import AlertsPage from '../../steps/mpop/pages/alerts.ts'
@@ -11,6 +11,9 @@ import { createCustodialEvent, CreatedEvent } from '@ministryofjustice/hmpps-pro
 import { createContact } from '@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/delius/contact/create-contact.mjs'
 import HomePage from '../../steps/mpop/pages/home.page.ts'
 import { login, loginIfNotAlready } from '../../steps/mpop/login.ts'
+import OverviewPage from '../../steps/mpop/pages/case/overview.page.ts'
+import ManageAppointmentsPage from '../../steps/mpop/pages/appointments/manage-appointment.page.ts'
+import NotePage from '../../steps/mpop/pages/appointments/note.page.ts'
 
 dotenv.config({ path: '.env' }) // Load environment variables
 
@@ -47,19 +50,89 @@ test.describe('Alerts page', () => {
   })
 
   test('Render the page', async() => {
+    test.setTimeout(120000)
     alerts = await navigateToAlerts(page)
     await alerts.checkOnPage()
   })
 
   test('Check alert added', async() => {
+    test.setTimeout(120000)
     await loginIfNotAlready(page)
     const home = new HomePage(page)
     const updatedCount = await home.getAlertsCount()
     expect(updatedCount).toBe(alertCount+1)
   })
 
-  test('Check alert links', async() => {
+  test('Check person link', async() => {
+    test.setTimeout(120000)
     alerts = await navigateToAlerts(page)
+    const row = alerts.getClass('govuk-table__row').filter({has: page.getByRole('cell', {name: `${person.lastName}, ${person.firstName} ${crn}`})})
+    await alerts.getQA('alertPerson', row).getByRole('link', {name: `${person.lastName}, ${person.firstName}`}).click()
+    const overviewPage = new OverviewPage(page)
+    expect(overviewPage.page.url()).toContain(crn)
+  })
+
+  test('Check activity link', async() => {
+    test.setTimeout(120000)
+    alerts = await navigateToAlerts(page)
+    const row = alerts.getClass('govuk-table__row').filter({has: page.getByRole('cell', {name: `${person.lastName}, ${person.firstName} ${crn}`})})
+    await alerts.getQA('alertActivity', row).getByRole('link', {name: "3 Way Meeting (Non NS)"}).click()
+    const managePage = new ManageAppointmentsPage(page)
+    expect(managePage.page.url()).toContain(crn)
+    await managePage.clickBackLink()
     await alerts.checkOnPage()
+  })
+
+  test('Check activity note', async() => {
+    test.setTimeout(120000)
+    alerts = await navigateToAlerts(page)
+    const row = alerts.getClass('govuk-table__row').filter({has: page.getByRole('cell', {name: `${person.lastName}, ${person.firstName} ${crn}`})})
+    await alerts.getQA('alertActivity', row).getByText('More information').click()
+    await alerts.getQA('alertActivity', row).getByRole('link', {name: "View full note"}).click()
+    const notePage = new NotePage(page)
+    await notePage.checkOnPage()
+    await notePage.clickBackLink()
+    await alerts.checkOnPage()
+  })
+
+  test('Check pagination', async() => {
+    test.setTimeout(120000)
+    alerts = await navigateToAlerts(page)
+    await alerts.pagination("Next")
+    expect(alerts.getQA("alertsCount")).toContainText('Showing 11 to 20')
+    await alerts.pagination(1)
+    expect(alerts.getQA("alertsCount")).toContainText('Showing 1 to 10')
+  })
+
+  test('Check select all alerts', async() => {
+    test.setTimeout(120000)
+    alerts = await navigateToAlerts(page)
+    await alerts.getQA("selectAllAlertsBtn").click()
+    const checkboxes : Locator[] = await alerts.page.getByRole('checkbox').all()
+    for (const checkbox of checkboxes){
+      expect(checkbox).toBeChecked()
+    }
+    await alerts.getQA("selectAllAlertsBtn").click()
+    for (const checkbox of checkboxes){
+      expect(checkbox).not.toBeChecked()
+    }
+  })
+
+  test('Check clear alert - none', async() => {
+    test.setTimeout(120000)
+    alerts = await navigateToAlerts(page)
+    await alerts.getQA("clearSelectedAlerts").click()
+    await expect(alerts.getClass('moj-alert moj-alert--error')).toContainText('Select an alert to clear it')
+  })
+
+  test('Check clear alert', async() => {
+    test.setTimeout(120000)
+    alerts = await navigateToAlerts(page)
+    const row = alerts.getClass('govuk-table__row').filter({has: page.getByRole('cell', {name: `${person.lastName}, ${person.firstName} ${crn}`})})
+    await row.getByRole('checkbox').click()
+    await alerts.getQA("clearSelectedAlerts").click()
+    await expect(alerts.getClass('moj-alert moj-alert--success')).toContainText('1 alert(s) cleared successfully')
+    const finalCount = await alerts.getAlertsCount()
+    expect(finalCount).toBe(alertCount)
   })
 })
