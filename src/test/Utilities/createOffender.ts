5f1e5f41dd75148eeaeeece51043bc1172e10365
Ravindra Kumar <ravindrakumar.killamsetty@justice.gov.uk>
2026-02-11 11:48:20 +0000

    MAN-1965: updated files

--- src/test/Utilities/createOffender.ts
+++ src/test/Utilities/createOffender.ts
@@ -1,9 +1,9 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-import { Page } from '@playwright/test'
-import { createOffender as deliusCreateOffender } from '@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/delius/offender/create-offender.mjs'
-import { Person } from '@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/delius/utils/person.mjs'
+import { Page } from "@playwright/test";
+import { createOffender as deliusCreateOffender } from "@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/delius/offender/create-offender.mjs";
+import { Person } from "@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/delius/utils/person.mjs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,38 +11,31 @@ const __dirname = path.dirname(__filename);
 const USER_FILE = path.join(__dirname, "../temp-user.json");
 const TMP_FILE = USER_FILE + ".tmp";
 
-export const createOffender = async (page: Page, person: Person, providerName?: string): Promise<string> => {
-    let crn
-    console.log(`Creating ${USER_FILE}`);
+export const createOffender = async (
+  page: Page,
+  person: Person,
+  providerName?: string,
+): Promise<string> => {
+  let crn;
+  try {
+    const fd = fs.openSync(TMP_FILE, "wx");
 
+    crn = await deliusCreateOffender(page, { person, providerName });
 
-    console.log(`fs.existsSync(USER_FILE) ${fs.existsSync(USER_FILE)}`);
-    try {
-        // Attempt atomic lock
-        const fd = fs.openSync(TMP_FILE, "wx");
+    fs.writeSync(fd, JSON.stringify({ crn }));
+    fs.closeSync(fd);
+    fs.renameSync(TMP_FILE, USER_FILE);
 
-        // 👑 Only ONE worker reaches here
-        crn = await deliusCreateOffender(page, {person, providerName})
-
-        fs.writeSync(fd, JSON.stringify({ crn }));
-        fs.closeSync(fd);
-        fs.renameSync(TMP_FILE, USER_FILE);
-
-        console.log("User created by this worker:", crn);
-        return crn;
-
-    } catch (err: any) {
-        console.error("EXCEPTION");
-        // Another worker already created it
-        if (err.code !== "EEXIST") throw err;
-
-        // Wait until file is written
-        while (!fs.existsSync(USER_FILE)) {
-            await new Promise(r => setTimeout(r, 50));
-        }
-        console.log("READING file from file :", USER_FILE);
-        const data = JSON.parse(fs.readFileSync(USER_FILE, "utf-8"));
-        console.log("User reused from file:", data.crn);
-        return data.crn;
+    console.log("USER CREATED, CRN: ", crn);
+    return crn;
+  } catch (err: any) {
+    console.error("EXCEPTION");
+    if (err.code !== "EEXIST") throw err;
+    while (!fs.existsSync(USER_FILE)) {
+      await new Promise((r) => setTimeout(r, 50));
     }
-}
+    const data = JSON.parse(fs.readFileSync(USER_FILE, "utf-8"));
+    console.log("READING CRN from file:", data.crn);
+    return data.crn;
+  }
+};
