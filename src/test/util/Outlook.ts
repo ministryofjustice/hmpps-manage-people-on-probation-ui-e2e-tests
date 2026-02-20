db02824bcc8bc74b1a67aeb77839e18cb636ee36
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-02-20 11:55:10 +0000

    refactor

--- src/test/util/Outlook.ts
+++ src/test/util/Outlook.ts
@@ -3,9 +3,8 @@ import { DateTime } from "luxon";
 import { getCalenderEvent, getClientToken, getExternalReference } from "./API";
 import { getUrn } from "./Common";
 
-export const checkOutlook = async(page: Page, crn: string, past: boolean) => {
+export const checkOutlook = async(page: Page, crn: string, token: string, past: boolean) => {
     const urn = getUrn(page)
-    const token = await getClientToken()
     const external = await getExternalReference(crn, urn, token)
     const [status, body] = await getCalenderEvent(external, token)
     if (past){
