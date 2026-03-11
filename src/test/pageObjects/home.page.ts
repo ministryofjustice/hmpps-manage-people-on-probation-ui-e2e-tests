be8cdc0c16b4dfaee0ecd4a56487a959c784b687
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-03-11 11:46:01 +0000

    MAN-2088

--- src/test/pageObjects/home.page.ts
+++ src/test/pageObjects/home.page.ts
@@ -1,6 +1,8 @@
 import { Page } from "@playwright/test";
 import MPopPage from "./page";
 import { MPOP_URL } from "../util/Data";
+import UpcomingAppointmentsPage from "./upcoming.page";
+import LogOutcomesPage from "./log-outcomes.page";
 
 export default class HomePage extends MPopPage {
     constructor(page: Page) {
@@ -28,6 +30,41 @@ export default class HomePage extends MPopPage {
 
     async returnToPage(){
         await this.usePrimaryNavigation('Home')
+        await this.checkOnPage()
+    }
+
+    async showUpcoming(){
+        await this.getQA('homepage-appointments').getByRole('button').click({position: {x:0, y:0}})
+    }
+    async noteUpcoming(): Promise<string[]> {
+        await this.showUpcoming()
+        const text = await this.getQA('homepage-appointments').getByRole('link', {name: /,/}).allTextContents()
+        return text.map(i => i.trim())
+    }
+    async checkUpcoming(){
+        const upcoming = await this.noteUpcoming()
+        await this.page.getByRole('link', {name: 'View all upcoming appointments'}).click()
+        const upcomingPage = new UpcomingAppointmentsPage(this.page)
+        await upcomingPage.checkOnPage()
+        await upcomingPage.checkTop5(upcoming)
+        await this.returnToPage()
+    }
+
+    async showOutcomes(){
+        await this.getQA('homepage-outcomes').getByRole('button').click({position: {x:0, y:0}})
+    }
+    async noteOutcomes(): Promise<string[]> {
+        await this.showOutcomes()
+        const text = await this.getQA('homepage-outcomes').getByRole('link', {name: /,/}).allTextContents()
+        return text.map(i => i.trim())
+    }
+    async checkOutcomes(){
+        const outcomes = await this.noteOutcomes()
+        await this.page.getByRole('link', {name: 'Log more outcomes'}).click()
+        const outcomesPage = new LogOutcomesPage(this.page)
+        await outcomesPage.checkOnPage()
+        await outcomesPage.checkTop5(outcomes)
+        await this.returnToPage()
     }
 
     async checkLinks(){
@@ -35,5 +72,9 @@ export default class HomePage extends MPopPage {
         await this.returnToPage()
         await this.page.getByRole('link', {name: 'View your cases'}).click()
         await this.returnToPage()
+        await this.checkUpcoming()
+        await this.checkOutcomes()
+
+
     }
 }
--- src/test/steps/pages/Home.steps.ts
+++ src/test/steps/pages/Home.steps.ts
@@ -1,5 +1,7 @@
 import {createBdd, DataTable} from "playwright-bdd";
 import {testContext} from "../../features/Fixtures";
+import OverviewPage from "../../pageObjects/Case/overview.page";
+import TierPage from "../../pageObjects/Case/tier.page";
 import HomePage from "../../pageObjects/home.page";
 
 const { Given, When, Then } = createBdd(testContext);
