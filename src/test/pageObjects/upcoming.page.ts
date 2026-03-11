8c1ec6c75885b7cd4ef2dba8f4cce6f9f81d31c2
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-03-11 11:46:01 +0000

    MAN-2088

--- src/test/pageObjects/upcoming.page.ts
+++ src/test/pageObjects/upcoming.page.ts
@@ -15,4 +15,10 @@ export default class UpcomingAppointmentsPage extends MPopPage {
         const top5 = text.slice(0,5).map(i => i.trim())
         expect(upcoming[0]).toEqual(top5[0]) //only check top1 atm due to sorting issues to be resolved in backend
     }
+
+    async checkTop5(upcoming: string[]){
+        const text = await this.getClass('moj-scrollable-pane').getByRole('link', {name: /,/}).allTextContents()
+        const top5 = text.slice(0,5).map(i => i.trim())
+        expect(upcoming[0]).toEqual(top5[0]) //only check top1 atm due to sorting issues to be resolved in backend
+    }
 }
