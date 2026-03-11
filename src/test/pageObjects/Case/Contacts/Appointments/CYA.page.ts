e1a933041597fb25d5bf96de3937143a9d703f0c
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-03-11 13:25:55 +0000

    more efficient page checks

--- src/test/pageObjects/Case/Contacts/Appointments/CYA.page.ts
+++ src/test/pageObjects/Case/Contacts/Appointments/CYA.page.ts
@@ -163,7 +163,7 @@ export default class CYAPage extends ContactPage {
                 else if (changes.sensitivity){
                     await this.clickSummaryAction(rows.indexOf("Sensitivity"))
                 }
-            }
+            } 
             if (newPast){
                 const addNotePage = new AddNotePage(this.page)
                 await addNotePage.assertOnPage()
