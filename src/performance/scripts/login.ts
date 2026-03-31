c66a5c33e3298f7f5ca0379028524fc88aad2208
Amardeep Chimber <amardeep.chimber@justice.gov.uk>
2026-03-31 20:37:54 +0100

    MTRSB11: add gatling template

--- src/performance/scripts/login.ts
+++ src/performance/scripts/login.ts
@@ -4,8 +4,8 @@ export const loginAndGetCookies = async (page: Page) => {
     await page.goto("http://localhost:3000")
     if (await page.title() !== 'Manage people on probation') {
         await expect(page).toHaveTitle(/HMPPS Digital Services - Sign in/)
-        await page.fill('#username', process.env.DELIUS_USERNAME!)
-        await page.fill('#password', process.env.DELIUS_PASSWORD!)
+        await page.fill('#username', process.env.DELIUS_USERNAME)
+        await page.fill('#password', process.env.DELIUS_PASSWORD)
         await page.click('#submit')
     }
     await expect(page.locator('[data-qa="pageHeading"]')).toContainText('Manage people on probation')
