3b5cd264cb4219345bb8fe5227a75cfedf095b49
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-04-16 12:23:24 +0100

    MAN-2445

--- src/test/util/Outlook.ts
+++ src/test/util/Outlook.ts
@@ -3,8 +3,9 @@ import { DateTime } from "luxon";
 import { getCalenderEvent, getClientToken, getExternalReference } from "./API";
 import { getUrn } from "./Common";
 import { CaseType } from "../features/Fixtures";
+import { MpopDateTime } from "./DateTime";
 
-export const checkOutlook = async(page: Page, caseInfo: CaseType, token: string, past: boolean) => {
+export const checkOutlook = async(page: Page, caseInfo: CaseType, token: string, past: boolean, dateTime: MpopDateTime) => {
     const urn = getUrn(page)
     const external = await getExternalReference(caseInfo.crn, urn, token)
     const [status, body] = await getCalenderEvent(external, token)
@@ -14,7 +15,10 @@ export const checkOutlook = async(page: Page, caseInfo: CaseType, token: string,
     } else {
         expect(status).toBe(200)
         const message = body.subject
-        console.log(message)
-        expect(message).toContain(`with ${caseInfo.person.firstName} ${caseInfo.person.lastName}`)
+        const startDate = DateTime.fromISO(body.startDate).toFormat('HH:mm')
+        const endDate = DateTime.fromISO(body.endDate).toFormat('HH:mm')
+        expect(startDate).toBe(dateTime.startTime)
+        expect(endDate).toBe(dateTime.endTime)
+        expect(message).toContain(`${caseInfo.person.firstName[0]}. ${caseInfo.person.lastName}`)
     }
 }
