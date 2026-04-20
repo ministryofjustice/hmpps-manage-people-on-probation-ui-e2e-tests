cba2aefa46b36b0e51fb8203694d68c7b865f73f
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-04-20 17:58:52 +0100

    ESUP-SPO-approval-page

---
+++ src/test/pageObjects/Case/Contacts/Checkins/SetUp/spo-approval.page.ts
@@ -0,0 +1,14 @@
+import { expect, Page } from "@playwright/test";
+import ContactPage from "../../Contacts/contact.page";
+
+export default class SPOApprovalPage extends ContactPage {
+    constructor(page: Page, crn?: string, uuid?: string) {
+        super(page, "Check you've got approval before you sign", crn, uuid);
+    }
+
+    async completePage() {
+        await this.page.getByRole('checkbox').check()
+        await this.submit()
+    }
+
+}
