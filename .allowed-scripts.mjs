bd4793f36f8af71a967fd123ac53d07f9946d3e6
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-04-23 15:35:13 +0100

    lint

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -1,12 +1,12 @@
-import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
+import { configureAllowedScripts } from "@ministryofjustice/hmpps-npm-script-allowlist";
 
 export default configureAllowedScripts({
   allowlist: {
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
-    'node_modules/esbuild@0.27.4': 'ALLOW',
+    "node_modules/esbuild@0.27.4": "ALLOW",
     // Needed by jest for running tests in watch mode
-    'node_modules/fsevents@2.3.2': 'ALLOW',
+    "node_modules/fsevents@2.3.2": "ALLOW",
     // Need by playwright for detecting file system changes during test runs
-    'node_modules/tsx/node_modules/fsevents@2.3.3': 'ALLOW',
+    "node_modules/tsx/node_modules/fsevents@2.3.3": "ALLOW",
   },
-})
+});
