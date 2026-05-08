ebdf1f57051408e60770357443959a9e81ba5d75
karuna kanumuri <karuna.kanumuri@justice.gov.uk>
2026-05-08 16:11:37 +0100

    removed unwanted code

--- src/test/pageObjects/Case/risk-and-plan.page.ts
+++ src/test/pageObjects/Case/risk-and-plan.page.ts
@@ -1,33 +1,24 @@
 import { expect, Page } from "@playwright/test";
 import * as dotenv from "dotenv";
-import { caseNavigation } from "../../util/Navigation";
 import CasePage from "./casepage";
 
 dotenv.config({ path: ".env" });
-const MPOP_URL = process.env.MANAGE_PEOPLE_ON_PROBATION_URL;
 
 export default class RiskAndPlanPage extends CasePage {
   constructor(page: Page, crn?: string) {
     super(page, "Risk and plan", crn);
   }
 
-  async goTo(crn?: string) {
-      console.log("CRN:---", crn);
-    await this.page.goto(`${MPOP_URL}/case/${(crn ?? this.crn)!}/risk`);
-  }
-
-  async navigateTo(crn?: string) {
-    await caseNavigation(this.page, (crn ?? this.crn)!, "riskTab");
-  }
-
   async checkRiskPlanSectionAndLink() {
-    const link = this.page.getByRole("link", {name: /View the sentence plan/i,});
+    const link = this.page.getByRole("link", {
+      name: /View the sentence plan/i,
+    });
     await expect(link).toBeVisible();
     const [newPage] = await Promise.all([
-        this.page.context().waitForEvent("page"),
-        link.click(),
+      this.page.context().waitForEvent("page"),
+      link.click(),
     ]);
     await newPage.waitForLoadState();
     await expect(newPage).toHaveURL(/sentence-plan/);
- }
+  }
 }
--- src/test/steps/journeys/Risk-And-Plan.steps.ts
+++ src/test/steps/journeys/Risk-And-Plan.steps.ts
@@ -7,22 +7,22 @@ import RiskPage from "../../pageObjects/Case/risk.page";
 const { When, Then } = createBdd(testContext);
 
 When("I navigate to the Risk and Plan tab", async ({ ctx }) => {
-    const page = ctx.base.page;
-    const overviewPage = new OverviewPage(page);
-    await overviewPage.assertOnPage();
-    await Promise.all([
-        page.waitForURL(/risk/i),
-        overviewPage.useSubNavigation("riskTab"),
-    ]);
+  const page = ctx.base.page;
+  const overviewPage = new OverviewPage(page);
+  await overviewPage.assertOnPage();
+  await Promise.all([
+    page.waitForURL(/risk/i),
+    overviewPage.useSubNavigation("riskTab"),
+  ]);
 });
 
 Then("I can see the Sentence Plan link", async ({ ctx }) => {
-    const page = ctx.base.page;
-    const riskPage = new RiskPage(page);
-    await riskPage.checkSections();
-    await riskPage.checkRiskFlagLinks();
-    // Risk and Plan page, verify Sentence plan link exists for this CRN
-    const riskAndPlanPage = new RiskAndPlanPage(page);
-    await riskAndPlanPage.assertOnPage();
-    await riskAndPlanPage.checkRiskPlanSectionAndLink();
+  const page = ctx.base.page;
+  const riskPage = new RiskPage(page);
+  await riskPage.checkSections();
+  await riskPage.checkRiskFlagLinks();
+  // Risk and Plan page, verify Sentence plan link exists for this CRN
+  const riskAndPlanPage = new RiskAndPlanPage(page);
+  await riskAndPlanPage.assertOnPage();
+  await riskAndPlanPage.checkRiskPlanSectionAndLink();
 });
