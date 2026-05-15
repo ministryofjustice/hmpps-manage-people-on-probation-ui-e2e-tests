4c1d1f93d37e9d1e56cda5746b95b9797f959788
karuna kanumuri <karuna.kanumuri@justice.gov.uk>
2026-05-15 19:43:03 +0100

    log out come jurney

---
+++ src/test/pageObjects/Case/Contacts/Appointments/log-appointment-outcome.page.ts
@@ -0,0 +1,29 @@
+import { expect, Page } from "@playwright/test";
+import ContactPage from "../Contacts/contact.page";
+
+export default class AttendedCompliedPage extends ContactPage {
+  constructor(page: Page, crn?: string, uuid?: string) {
+    super(page, "attended and complied", crn, uuid);
+  }
+
+  async completePage() {
+    await this.page.getByRole("checkbox").check();
+    await this.submit();
+  }
+}
+
+export class NewAttendedCompliedPage extends ContactPage {
+  constructor(page: Page, crn?: string, uuid?: string) {
+    super(page, "What was the outcome of this appointment?", crn, uuid);
+  }
+  async completePage() {
+    const radioButton = this.page.getByRole("radio", {
+      name: "Attended",
+      exact: true,
+    });
+    await expect(radioButton).toBeVisible();
+    await radioButton.check();
+    await expect(radioButton).toBeChecked();
+    await this.submit();
+  }
+}
