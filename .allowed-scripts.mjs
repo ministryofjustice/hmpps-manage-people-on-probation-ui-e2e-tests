2ce486de6b7b65a79a01f94b2ae22f90c74176c9
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-05-20 12:29:09 +0100

    allow scripts - playwright version must allign with probation integration

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,12 +3,10 @@ import { configureAllowedScripts } from "@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
-    "node_modules/esbuild@0.27.4": "ALLOW",
+    "node_modules/esbuild@0.28.0": "ALLOW",
     // Needed by jest for running tests in watch mode
     "node_modules/fsevents@2.3.2": "ALLOW",
     // Need by playwright for detecting file system changes during test runs
     "node_modules/tsx/node_modules/fsevents@2.3.3": "ALLOW",
-    // Needed by gatling for performance testing
-    "node_modules/@gatling.io/cli/node_modules/esbuild@0.28.0": "ALLOW",
   },
 });
