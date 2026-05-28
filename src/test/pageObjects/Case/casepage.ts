959fd8beb071f304fd9e81c90e53d94d999c3e0e
rajmoj <dheeraj.bharti@justice.gov.uk>
2026-05-28 18:08:20 +0100

    Man 2616 - Added test for appointment outcomes (#133)
    * MAN-2327 fixed lint issues
    
    * MAN-2327 fixed lint issues
    
    * MAN-2327 fixed lint issues
    
    * MAN-2327 fixed lint issues
    
    * MAN-2617 Added test
    
    * MAN-2617 removed commented out code
    
    * MAN-2617 added package-lock.json
    
    * MAN-2617 added missing method to casepage.ts
    
    * MAN-2617 added missing method to casepage.ts
    
    * MAN-2617 fixed linting issues
    
    * MAN-2617 fixed linting issues

--- src/test/pageObjects/Case/casepage.ts
+++ src/test/pageObjects/Case/casepage.ts
@@ -1,6 +1,5 @@
 import { expect, Page } from "@playwright/test";
 import MPopPage from "../page";
-import UpcomingAppointmentsPage from "../upcoming.page";
 
 export default abstract class CasePage extends MPopPage {
   readonly crn?: string;
@@ -8,30 +7,6 @@ export default abstract class CasePage extends MPopPage {
   protected constructor(page: Page, title?: string | RegExp, crn?: string) {
     super(page, title);
     this.crn = crn;
-  }
-
-  async getNewestAppointment() {
-    try {
-      await this.clickLink("View all upcoming appointments.");
-      const upcomingAppointment = new UpcomingAppointmentsPage(this.page);
-      await upcomingAppointment.assertOnPage();
-      //sort descending
-      await upcomingAppointment.page
-        .getByRole("button", { name: "Date" })
-        .click();
-      await upcomingAppointment.page
-        .getByRole("button", { name: "Date" })
-        .click();
-      await this.getQA("upcomingAppointments")
-        .getByRole("link", { name: /Manage/ })
-        .first()
-        .click();
-    } catch {
-      await this.getQA("upcomingAppointmentsSection")
-        .getByRole("link", { name: /Manage/ })
-        .last()
-        .click();
-    }
   }
 
   async assertOnPage(allowRestricted: boolean = true) {
@@ -70,5 +45,25 @@ export default abstract class CasePage extends MPopPage {
   async checkPopHeader(crn?: string) {
     const checkCrn = (crn ?? this.crn)!;
     await this.checkQA("crn", checkCrn);
+  }
+
+  async assertTextOnOverviewPage(expectedText: string) {
+    await expect(
+      this.page.locator(`[class=govuk-heading-m]`).nth(1),
+    ).toContainText(expectedText);
+  }
+
+  async assertLinkTextOnOverviewPage(expectedHref: string) {
+    const href = await this.page
+      .locator(".govuk-notification-banner__content ul li a")
+      .first()
+      .getAttribute("href");
+    expect(href).toEqual(expectedHref);
+  }
+
+  async selectOutcomeLink() {
+    await this.page
+      .locator(".govuk-notification-banner__content ul li a")
+      .click();
   }
 }
