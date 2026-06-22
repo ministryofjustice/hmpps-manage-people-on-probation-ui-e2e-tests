2d3311fb41c05f3b4431e8644112a4a3abf35142
rajmoj <dheeraj.bharti@justice.gov.uk>
2026-06-22 14:16:46 +0100

    MAN-2617 added tests for sentence and overview pages (#163)
    * MAN-2617 added tests for sentence and overview pages
    
    * MAN-2617 Fixed lint issues

--- src/test/pageObjects/Case/sentence.page.ts
+++ src/test/pageObjects/Case/sentence.page.ts
@@ -501,6 +501,38 @@ export default class SentencePage extends CasePage {
     await sentencePage.useBreadcrumbs(2);
   }
 
+  async selectLink(linkName: string) {
+    if (linkName == "ORA Community Order")
+      await this.page
+        .getByRole("link", { name: /ORA Community Order/ })
+        .click();
+    else if (linkName == "ORA Adult Custody (inc PSS)")
+      await this.page.getByRole("link", { name: /ORA Adult Custody/ }).click();
+  }
+
+  async assertRequirementText(expectedText: string) {
+    if (expectedText == "requirements")
+      await expect(this.getQA("requirementsLabel")).toContainText(expectedText);
+    else if (expectedText == "Licence conditions")
+      await expect(this.getQA("licenceConditionsLabel")).toContainText(
+        expectedText,
+      );
+  }
+
+  async assertGPSDataLink(expectedLinkText: string) {
+    if (
+      expectedLinkText ==
+      "Location Monitoring(GPS Tagging) - Monitored Appointment"
+    )
+      await expect(this.getQA("requirementsEMDILink")).toContainText(
+        expectedLinkText,
+      );
+    else if (expectedLinkText == "View GPS location monitoring data")
+      await expect(this.getQA("licencesEMDILink")).toContainText(
+        expectedLinkText,
+      );
+  }
+
   async checkLinks(linkName: string) {
     switch (linkName) {
       case "Probation history":
