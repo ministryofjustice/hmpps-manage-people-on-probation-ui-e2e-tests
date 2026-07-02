d2dbb7c0a524ae4b4bf9f7e9f448a61233d0e43e
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-07-02 14:17:38 +0100

    fix allowlisst

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,7 +3,7 @@ import { configureAllowedScripts } from "@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
-    "node_modules/esbuild@0.28.0": "ALLOW",
+    "node_modules/esbuild@0.28.1": "ALLOW",
     // Needed by jest for running tests in watch mode
     "node_modules/fsevents@2.3.2": "ALLOW",
     // Need by playwright for detecting file system changes during test runs
