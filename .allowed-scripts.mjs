a183f8d71873a738bd02f83fb109e78826f2166e
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-09-17 11:06:17 +0100

    empty

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -4,8 +4,6 @@ export default configureAllowedScripts({
   allowlist: {
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
     "node_modules/esbuild@0.28.2": "ALLOW",
-    // Needed by jest for running tests in watch mode
-    "node_modules/fsevents@2.3.2": "ALLOW",
     // Need by playwright for detecting file system changes during test runs
     "node_modules/tsx/node_modules/fsevents@2.3.3": "ALLOW",
   },
